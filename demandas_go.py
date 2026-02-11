a2dbc5110cd33b2d2a6a20f318a92558157a5723
butinhoni <guilherme.of.fernandes@gmail.com>
2026-02-11 09:59:36 -0400

    correcao no formato de data e adicao de rodovia e observacao

--- demandas_go.py
+++ demandas_go.py
@@ -6,23 +6,27 @@ from planilhas import os_go
 df = os_go()
 telefones = numeros()
 
-df = df[["DESCRIÇÃO", "DATA\nVENCIMENTO", "STATUS"]]
+df = df[["DESCRIÇÃO", "RODOVIA", "DATA\nVENCIMENTO", "STATUS", "OBSERVAÇÕES"]]
+df["RODOVIA"] = df["RODOVIA"].ffill()
 df = df[df["STATUS"] != "Atendido"]
 df = df[df["STATUS"] != "Cancelada"]
-df.columns = ["desc", "vencimento", "status"]
+df.columns = ["desc", "rod", "vencimento", "status", "obs"]
 df["vencimento"] = pd.to_datetime(df["vencimento"], errors="coerce")
 df = df.dropna(subset="desc")
 df = df.dropna(subset="vencimento")
+df["obs"] = df["obs"].fillna("")
 
 text = "Olá equipe, segue relação das ordens de serviço em aberto no contrato GO Lote 3 \n\n"
 for i, row in df.iterrows():
+    data_format = row["vencimento"].strftime("%d/%m/%Y")
     desc = f"*Descrição:* {row['desc']}\n"
-    venc = f"*Vencimento:* {row['vencimento']}\n"
+    rod = f"*Rodovia:* {row['rod']}\n"
+    venc = f"*Vencimento:* {data_format}\n"
     status = f"*Status:* {row['status']}\n"
+    obs = f"*Obs:* {row['obs']}\n" if row["obs"] != "" else ""
     sep = "----------------------------- -------------------------------\n\n"
-    text = text + desc + venc + status + sep
+    text = text + desc + rod + venc + status + obs + sep
 print(text)
-
 msg = subprocess.run(
     ["./send_message.sh", "send_message", telefones["Diogo"], text],
     capture_output=True,
